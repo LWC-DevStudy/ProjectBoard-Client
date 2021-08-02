17c4f7769efc1afc1b2993d26fbbcb466e532e9e
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-08-02 23:22:25 +0900

    🎉 Input 엘리먼트 생성

--- lwc-board/src/elements/Input.js
+++ lwc-board/src/elements/Input.js
@@ -1,7 +1,66 @@
-import React from 'react';
+import styled from 'styled-components';
 
-function Input() {
-  return <div></div>;
-}
+const Input = ({
+  id,
+  type,
+  name,
+  value,
+  placeholder,
+  _onChange,
+  _onKeyPress,
+  ...props
+}) => {
+  return (
+    <InputStyle
+      id={id}
+      value={value}
+      placeholder={placeholder}
+      type={type}
+      onChange={_onChange}
+      onKeyPress={_onKeyPress}
+      name={name}
+      {...props}
+    />
+  );
+};
 
+Input.defaultProps = {
+  _onChange: () => {},
+  _onKeyPress: () => {},
+  type: 'text',
+  placeholder: '',
+  width: '100%',
+  height: '48px',
+  bgColor: 'white',
+  padding: '0 12px',
+  radius: '14px',
+  border: '1px solid #E7E7E7',
+};
+
+const InputStyle = styled.input`
+  border: ${(props) => props.border};
+  background: none;
+  border-radius: ${(props) => props.radius};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  padding: ${(props) => props.padding};
+  background-color: ${(props) => props.bgColor};
+  color: ${(props) => props.color};
+  font-size: ${(props) => props.fontSize};
+  box-shadow: ${(props) => props.shadow};
+  box-sizing: border-box;
+  &:focus {
+    outline: none;
+  }
+  &::placeholder {
+    color: ${(props) => props.color};
+  }
+  &::-webkit-input-placeholder {
+    color: ${(props) => props.color};
+  }
+  &:-ms-input-placeholder {
+    color: ${(props) => props.color};
+  }
+  ${(props) => props.addstyle}
+`;
 export default Input;
