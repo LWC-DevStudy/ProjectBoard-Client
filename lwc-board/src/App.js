dda939b616cf13cd908da13761b8b69ea99a0181
sunm-in <sunm309@naver.com>
2021-08-02 22:04:02 +0900

    Create React Project

---
+++ lwc-board/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// router
+import Route from 'react-router-dom';
+
+// redux
+
+// components
+
+// pages
+
+function App() {
+  return <></>;
+}
+
+export default App;
