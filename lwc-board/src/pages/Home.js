7720f1512696d975e76e9ba36df28703b5d12f03
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-08-03 22:37:15 +0900

    🔀merge하기위한 push

--- lwc-board/src/pages/Home.js
+++ lwc-board/src/pages/Home.js
@@ -1,12 +1,23 @@
 import React from 'react';
+// components
+import Header from '../components/Header';
 
 // elements
 import Grid from '../elements/Grid';
-
+import Text from '../elements/Text';
+import Button from '../elements/Button';
 function Home() {
   return (
     <React.Fragment>
-      <Grid bgColor="#C2B6F2" height="100vh"></Grid>
+      <Grid bgColor="white" height="100vh" padding="8px">
+        <Grid display="flex" coloumn="false" cursor="pointer">
+          <Button fontSize="10px">로그인</Button>
+          <Button fontSize="10px">회원가입</Button>
+        </Grid>
+        <Header>
+          <Text>일일계획 세우기</Text>
+        </Header>
+      </Grid>
     </React.Fragment>
   );
 }
